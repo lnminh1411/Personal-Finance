a1b185d25e09b1945d25b5ef338ab42e2b20c45c
lnminh1411 <minecraftgamemcgame2@gmail.com>
2025-03-02 14:25:14 +0700

    Basic UI, no functionalities yet

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function maincalculation(a, b, c, d) {
     let p = Math.ceil((a*12*1.04*(Math.pow((1+b), c)-1))/(b*Math.pow((1+b), c)));
     return Math.ceil(p/((1-Math.pow((1+(b)), (d+1)))/(-b)));
 }
-
 function getprice() {
 
 } 
